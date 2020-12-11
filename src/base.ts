9d09eee3d41413e7f5720257b2ecb0925a7c608c
k1w1 <chris@aha.io>
2020-12-11 12:53:20 -0800

    Support -d for development mode In development mode provide full URL from netrc file.

--- src/base.ts
+++ src/base.ts
@@ -1,6 +1,6 @@
-import Command, {flags, ParserOutput} from '@oclif/command';
-import AhaAPI from './api';
-import netrc from 'netrc-parser';
+import Command, { flags, ParserOutput } from "@oclif/command";
+import AhaAPI from "./api";
+import netrc from "netrc-parser";
 import Debug from "debug";
 const debug = Debug("aha");
 
@@ -9,19 +9,20 @@ abstract class BaseCommand extends Command {
   flags: ParserOutput;
 
   static flags = {
-    domain: flags.string({
-      char: 'd',
-      description: 'Aha! domain to connect to (mycompany.aha.io)'
+    server: flags.string({
+      char: "s",
+      description: "Aha! server to connect to (mycompany.aha.io)",
     }),
     development: flags.boolean({
-      description: 'whether to treat domains as development servers'
+      char: "d",
+      description: "whether to treat domains as development servers",
     }),
-  }
+  };
 
   async init() {
     // do some initialization
-    const {flags} = this.parse(this.constructor)
-    this.flags = flags
+    const { flags } = this.parse(this.constructor);
+    this.flags = flags;
     this._api = null;
   }
 
@@ -29,24 +30,24 @@ abstract class BaseCommand extends Command {
     if (this._api) {
       return this._api;
     } else {
-      this._api = this.prepareAPI()
-      return this._api
+      this._api = this.prepareAPI();
+      return this._api;
     }
   }
 
   prepareAPI(): AhaAPI {
-    const { domain, token } = this.loadAuth();
+    const { server, token } = this.loadAuth();
     let baseURL;
 
     if (this.flags.development) {
-      baseURL = `http://${domain.replace(/\.aha\.io/, '.lvh.me')}:3000`;
+      baseURL = `http://${server}`;
     } else {
-      baseURL = `https://${domain}`;
+      baseURL = `https://${server}`;
     }
-    const newAPI = new AhaAPI({ baseURL })
+    const newAPI = new AhaAPI({ baseURL });
     newAPI.defaults.headers = {
       authorization: `Bearer ${token}`,
-    }
+    };
     return newAPI;
   }
   resetAPI() {
@@ -54,36 +55,39 @@ abstract class BaseCommand extends Command {
   }
 
   loadAuth() {
-    netrc.loadSync()
+    netrc.loadSync();
 
-    let domain = this.flags.domain;
+    let server = this.flags.server;
     let token = null;
     let email = null;
-    if (domain) {
+    if (server) {
       // User specified the domain on the command line.
-      if (!netrc.machines[domain]) {
-        throw new Error(`No credentials found for ${domain}, use "aha auth:login" to login first`);
+      if (!netrc.machines[server]) {
+        throw new Error(
+          `No credentials found for ${server}, use "aha auth:login" to login first`
+        );
       }
-      token = netrc.machines[domain].token;
-      email = netrc.machines[domain].email;
+      token = netrc.machines[server].token;
+      email = netrc.machines[server].email;
     } else {
       // Use the first .aha.io domain we find.
-      for (domain in netrc.machines) {
-        if (domain.match(/.+\.aha\.io$/)) {
-          debug(`using credentials for ${domain}`)
-          token = netrc.machines[domain].token;
-          email = netrc.machines[domain].email;
+      for (server in netrc.machines) {
+        if (server.match(/.+\.aha\.io$/)) {
+          debug(`using credentials for ${server}`);
+          token = netrc.machines[server].token;
+          email = netrc.machines[server].email;
           break;
         }
       }
     }
 
     if (!token)
-      throw new Error(`No credentials found, use "aha auth:login" to login first`);
+      throw new Error(
+        `No credentials found, use "aha auth:login" to login first`
+      );
 
-    return {domain, token, email};
+    return { server, token, email };
   }
-
 }
 
 export default BaseCommand;
