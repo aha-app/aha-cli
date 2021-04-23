8a858c37f2cd12fa7e35b78d84b63b29e8870d83
Justin Weiss <justin@justinweiss.com>
2021-04-23 16:00:57 -0700

    Add a README template

--- src/commands/extension/create.ts
+++ src/commands/extension/create.ts
@@ -62,6 +62,11 @@ export default class Create extends BaseCommand {
       packageTemplate(identifier, name, author)
     );
 
+    fs.writeFileSync(
+      `${directoryName}/README.md`,
+      readmeTemplate(name)
+    );
+
     const modulePath = path.join(
       directoryName,
       'node_modules',
@@ -101,6 +106,65 @@ export default class Create extends BaseCommand {
 
     ux.action.stop(`Extension created in directory '${directoryName}'`);
   }
+}
+
+function readmeTemplate(name: string) {
+    return `# ${name}
+
+TODO: Supply a short overview of the functionality the extension brings to Aha! Develop
+This [Aha! Develop](https://www.aha.io/develop/overview) extension enables...
+
+It provides these contributions:
+
+TODO: Add a contribution list here. These should briefly describe each of the features provided by this extension.
+
+## Demo
+
+TODO: Add a short .gif screen recording showing some of the highlights of your extension. This can be recorded using a tool like [LICECap](https://www.cockos.com/licecap/).
+
+![demo](demo.gif)
+
+## Installing the extension
+
+**Note: In order to install an extension into your Aha! Develop account, you must be an account administrator.**
+
+# TODO: Fill in a link to your built extension package
+Install the ${name} extension by clicking [here](https://secure.aha.io/settings/account/extensions/install?url=).
+
+## Working on the extension
+
+Install \`aha-cli\`:
+
+\`\`\`sh
+ npm install -g @aha-app/aha-cli
+\`\`\`
+
+Clone the repo:
+
+TODO: Add the repository URL here
+\`\`\`sh
+git clone ...
+\`\`\`
+
+**Note: In order to install an extension into your Aha! Develop account, you must be an account administrator.**
+
+Install the extension into Aha! and set up a watcher:
+
+\`\`\`sh
+aha extension:install
+aha extension:watch
+\`\`\`
+
+Now, any change you make inside your working copy will automatically take effect in your Aha! account.
+
+When you are finished working on your extension, build it so that others can install it through its URL:
+
+\`\`\`sh
+aha extension:build
+\`\`\`
+
+To learn more about developing Aha! Develop extensions, including the API reference, the full documentation is located here: [Aha! Develop Extension API]()
+`
 }
 
 function packageTemplate(identifier: string, name: string, author: string) {
