ad466171d0825a68c2626eb7e0dcfd5668e0e75a
Jeremy Wells <jemmyw@gmail.com>
2021-05-07 11:41:52 +1200

    Add the noCache flag to the build command

--- src/commands/extension/build.ts
+++ src/commands/extension/build.ts
@@ -1,4 +1,5 @@
 import BaseCommand from '../../base';
+import { flags } from '@oclif/command';
 import { buildExtension } from '../../utils/extension-utils';
 
 export default class Install extends BaseCommand {
@@ -6,9 +7,12 @@ export default class Install extends BaseCommand {
 
   static flags = {
     ...BaseCommand.flags,
+    noCache: flags.boolean({
+      description: 'skip cached http imports',
+    }),
   };
 
   async run() {
-    await buildExtension(this);
+    await buildExtension(this, this.flags.noCache);
   }
 }
