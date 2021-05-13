1416341239a5a2203d4425b31adea6490f7a3f34
Percy Hanna <percy@aha.io>
2021-05-13 12:36:55 -0600

    Convert API/auth loading to async
    In order to use `inquirer`, it needs to load the auth/API asynchronously.

--- src/base.ts
+++ src/base.ts
@@ -1,9 +1,17 @@
 import Command, { flags } from '@oclif/command';
 import { Input } from '@oclif/parser';
 import netrc from 'netrc-parser';
+import { prompt } from 'inquirer';
 import AhaAPI from './api';
 
+interface ApiAuth {
+  token: string;
+  url: string;
+}
+
 abstract class BaseCommand extends Command {
+  static needsAuth = false;
+
   static flags = {
     subdomain: flags.string({
       char: 's',
@@ -13,12 +21,19 @@ abstract class BaseCommand extends Command {
 
   flags?: any;
 
+  _auth?: ApiAuth;
+
   _api?: AhaAPI;
 
   async init() {
     // do some initialization
     const { flags } = this.parse(this.constructor as Input<any>);
     this.flags = flags;
+
+    const { needsAuth } = this.constructor as typeof BaseCommand;
+    if (needsAuth) {
+      await this.initAPI();
+    }
   }
 
   get api(): AhaAPI {
@@ -26,56 +41,71 @@ abstract class BaseCommand extends Command {
       return this._api;
     }
 
-    this._api = this.prepareAPI();
-    return this._api;
+    throw new Error(
+      'API not initialized. Set static needsAuth = true in your command.'
+    );
   }
 
-  prepareAPI(): AhaAPI {
-    const { url, token } = this.loadAuth();
+  async initAPI(): Promise<AhaAPI> {
+    const { url, token } = await this.loadAuth();
 
-    const newAPI = new AhaAPI({ baseURL: url });
-    newAPI.defaults.headers = {
+    this._api = new AhaAPI({ baseURL: url });
+    this._api.defaults.headers = {
       authorization: `Bearer ${token}`,
     };
-    return newAPI;
+    return this._api;
   }
 
   resetAPI() {
     this._api = undefined;
+    this._auth = undefined;
   }
 
-  loadAuth(): { url: string; token: string } {
-    let subdomain = this.flags?.subdomain;
-    let token = null;
-    let url = null;
+  async loadAuth(): Promise<ApiAuth> {
+    if (this._auth) {
+      return this._auth;
+    }
+
+    const subdomain = this.flags?.subdomain;
+    let machine;
 
     netrc.loadSync();
+    const { machines } = netrc;
     if (subdomain) {
       // User specified the domain on the command line.
-      if (!netrc.machines[subdomain]) {
+      machine = machines[subdomain];
+      if (!machine) {
         throw new Error(
           `No credentials found for ${subdomain}, use "aha auth:login -s ${subdomain}" to login first`
         );
       }
-      token = netrc.machines[subdomain].token;
-      url = netrc.machines[subdomain].url;
     } else {
-      // Use the first type=aha url we find.
-      for (subdomain in netrc.machines) {
-        if (netrc.machines[subdomain].type === 'aha') {
-          token = netrc.machines[subdomain].token;
-          url = netrc.machines[subdomain].url;
-          break;
-        }
+      const machineOptions = Object.keys(machines).filter(
+        key => machines[key].type === 'aha'
+      );
+      machine = machines[machineOptions[0]];
+
+      if (machineOptions.length > 1) {
+        ({ machine } = await prompt({
+          type: 'list',
+          name: 'machine',
+          message: 'Which Aha! account do you want to use?',
+          default: machine,
+          choices: machineOptions.map(key => ({
+            name: key,
+            value: machines[key],
+          })),
+        }));
       }
     }
 
-    if (!token || !url)
+    if (!machine || !machine.token || !machine.url)
       throw new Error(
         `No credentials found, use "aha auth:login" to login first`
       );
 
-    return { url, token };
+    this._auth = { url: machine.url, token: machine.token };
+    return this._auth;
   }
 
   // Catch all unhandled errors and display to the user in a reasonable way.
